3f44f10df1023520375d1a1a49036cb7a75247fb
chesnokov <chesnokov.am@phystech.edu>
2022-09-14 09:25:33 +0300

    Start iterating files

--- 01tech/0.sh
+++ 01tech/0.sh
@@ -1,38 +1,60 @@
 #!/usr/bin/env bash
 
-MAN3_PATH="$(find $MANPATH -type d -name "man3" | head -1)"
-
-#echo "Found $MAN3_PATH"
-
-while read FUNCTION
-do
-    #echo "$FUNCTION"
-    #Check end of input
-    if [ -z "$FUNCTION" ]
+check_and_unzip () {
+    if [ $1==".*.gz" ]
     then
-        break
-    fi 
-    #Check man3 file exictanse
-    FILE="$(find $MAN3_PATH -type f -name "$FUNCTION.3*")"
-    #echo $FILE
-    if [ -z $FILE ]
-    then
-        echo "---"
-        continue
-    fi
-    TMP_FILE="./tmp"
-    if [ $FILE==".*.gz" ]
-    then
-        #echo "unzip"
         TMP_FILE="./tmp.gz"
         cp $FILE $TMP_FILE 
         gunzip $TMP_FILE
         TMP_FILE=${TMP_FILE::-3}
+    else
+        TMP_FILE="./tmp"
+        cp $FILE $TMP_FILE
     fi
-    #echo $TMP_FILE
+    echo "$TMP_FILE"
+}
 
+find_function () {
+   for FILE in $1/*
+    do
+        if [ -d $FILE  ]
+        then
+            continue
+        fi
+        TMP_FILE="$(check_and_unzip $FILE)"
+        LINE="$(grep "\"FILE \*$2" $TMP_FILE | head -1)"
+        if [ ! -z "$LINE" ]
+        then
+            break
+        else
+            rm $TMP_FILE
+        fi
+    done 
+    if [ ! -z "$LINE" ]
+    then
+        echo "$TMP_FILE"
+    else
+        echo ""
+    fi
+}
+
+MAN3_PATH="$(find $MANPATH -type d -name "man3" | head -1)"
+
+while read FUNCTION
+do
+    if [ -z "$FUNCTION" ]
+    then
+        break
+    fi 
+
+    TMP_FILE="$(find_function $MAN3_PATH $FUNCTION)"
+    cat $TMP_FILE
+    if [ -z $TMP_FILE ]
+    then
+        echo "---"
+        continue
+    fi
     sed -n 's/[^<]*<\([^>]*\).*/\1/p' $TMP_FILE
 
-    #echo "delete"
     rm $TMP_FILE
 done
