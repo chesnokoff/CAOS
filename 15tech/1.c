b4d62eb95abff03f39c61611d57885d23d28d083
chesnokov <chesnokov.am@phystech.edu>
2022-12-20 22:10:39 +0300

    tech 15-1 was edited. Change sig_atomic_t to array of partial sums

--- 15tech/1.c
+++ 15tech/1.c
@@ -1,22 +1,20 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <bits/types/sig_atomic_t.h>
-
-volatile sig_atomic_t answer = 0;
 
 void* read_and_add(void* arg) {
+  int* sum = (int*) arg;
   int number = 0;
 
 #ifdef DEBUG
   fprintf(stderr, "Start reading\n");
 #endif
   while ((scanf("%d", &number)) > 0) {
-#ifdef DEBUG
-    fprintf(stderr, "Read: %d\n", number);
-#endif
-    answer += number;
+    *sum += number;
   }
+#ifdef DEBUG
+  fprintf(stderr, "End reading\n");
+#endif
   return NULL;
 }
 
@@ -28,8 +26,19 @@ int main(int argc, const char** argv) {
 
   long N = strtol(argv[1], NULL, 10);
 
+  pthread_attr_t pthread_attr = {};
+  pthread_attr_init(&pthread_attr);
+  pthread_attr_setstacksize(&pthread_attr, 4096);
+#ifdef DEBUG
+  fprintf(stderr, "Attribute was set\n");
+#endif
   pthread_t* threads = calloc(N, sizeof(pthread_t));
   if (NULL == threads) {
+    perror("Allocation error");
+    exit(1);
+  }
+  int* sums = calloc(N, sizeof(int));
+  if (NULL == sums) {
     perror("Allocation error");
     exit(1);
   }
@@ -38,16 +47,22 @@ int main(int argc, const char** argv) {
 #endif
 
   for (size_t i = 0; i < N; i++) {
-    if (0 > pthread_create(threads + i, NULL, read_and_add, NULL)) {
+    if (0 > pthread_create(threads + i, &pthread_attr, read_and_add, sums + i)) {
       perror("Creating new thread error");
       exit(1);
     }
   }
+
+  int answer = 0;
   for (int i = 0; i < N; i++) {
     if (0 > pthread_join(threads[i], NULL)) {
       perror("Waiting thread error");
       exit(1);
     }
+  }
+
+  for (int i = 0; i < N; i++) {
+    answer += sums[i];
   }
 
   printf("%d\n", answer);
@@ -56,6 +71,8 @@ int main(int argc, const char** argv) {
   fprintf(stderr, "Done\n");
 #endif
 
+  pthread_attr_destroy(&pthread_attr);
+  free(sums);
   free(threads);
   return 0;
 }
